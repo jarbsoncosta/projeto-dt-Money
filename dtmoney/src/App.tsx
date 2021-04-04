e383b242f148993cc9dfa78e77329ab7e041a6b8
jarbson costa <jarbsonfc@gmail.com>
2021-04-04 14:01:36 -0300

    definindo style global com styled-component

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,8 +1,10 @@
-
-
+import { GlobalStyle } from './styles/global'
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <h1 className="title">Hello World</h1>
+      <GlobalStyle />
+    </div>
   );
 }
 
