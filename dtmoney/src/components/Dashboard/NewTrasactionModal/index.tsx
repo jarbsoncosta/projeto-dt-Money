56040113e741691ddac273486da32f8eb01a43ea
jarbson costa <jarbsonfc@gmail.com>
2021-04-12 12:08:12 -0300

    colorindo botões do Model

--- dtmoney/src/components/Dashboard/NewTrasactionModal/index.tsx
+++ dtmoney/src/components/Dashboard/NewTrasactionModal/index.tsx
@@ -51,6 +51,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         type="button"
                         onClick={() => { setType('deposit') }}
                         isActive={type === 'deposit'}
+                        activeColor="green"
 
                     >
                         <img src={incomeImg} alt="Entrada" />
@@ -60,6 +61,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         type="button"
                         onClick={() => { setType('withdraw') }}
                         isActive={type === 'withdraw'}
+                        activeColor="red"
 
                     >
                         <img src={outcomeImg} alt="Saída" />
